f086079f93e964f6949ef212ea4c0edd413ab3f3
pankajyadav2006 <pankaj.yadav@adypu.edu.in>
2026-05-01 14:17:29 +0530

    Add root health check route

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
+app.get('/', (req, res) => {
+    res.send('Finance Manager API is running!');
+});
+
 // JWT Middleware
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
